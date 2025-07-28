093457ff97f79022173f4ad63caa3b4297d69cab
iljarobalkin <robalkin.ilja@gmail.com>
2025-07-28 22:12:26 +0300

    feat: add entities.go, cleanup main func

---
+++ entities.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func AddEntities() {
+
+	fmt.Println("Adding entities package")
+	fmt.Println("Adding structs for TODO managements")
+	fmt.Println("Adding relations")
+	fmt.Println("Finished adding entities")
+
+}
